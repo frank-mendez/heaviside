3a293035675eee20a4fd51d2ba2e0baee659edad
Frank Mendez <Frank Mendez>
2017-11-30 18:56:38 +0800

    feat(files): initialize development files

---
+++ index.php
@@ -0,0 +1,218 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Milwaukee Web Design Company and Digital Marketing Experts</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- SEO -->
+    <meta name="description" content="We&#039;re a Milwaukee web design company without a BORING description. We just get results. You&#039;ll definitely want to check out our free..."/>
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Milwaukee Web Design Company and Digital Marketing Experts" />
+    <meta property="og:description" content="We&#039;re a Milwaukee web design company without a BORING description. We just get results. You&#039;ll definitely want to check out our free..." />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="shortcut icon" href="//staging.heavisidegroup.com/wp-content/uploads/2015/05/favicon.ico">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  </head>
+  <body>
+    <?php include_once( 'views/layout/navbar.php' ); ?>
+    <div class="container-fluid main-banner">
+      <div class="row ">
+        <div class="col-sm-6 offset-sm-6 qoute-container">
+          <h4>WE BUILD WEBSITES THAT</h4>
+          <h1>BUILD BUSINESSES</h1>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h4>WE ARE A</h4>
+          <h3>Milwaukee web design company</h3>
+          <h2>that helps you get more customers.</h2>
+        </div>
+        <div class="col">
+          <ul>
+            <li>You don’t need an expensive brochure that no one sees.</li>
+            <li> You need your website working 24/7 to help your customers find you.</li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <img src="img/image-1.png">
+        </div>
+        <div class="col">
+          <img src="img/expertise-2017-1-1.png">
+          <img src="img/expertise-2016-1-1.png">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <p>WHAT WE DO</p>
+          <h2>Milwaukee Digital Marketing That Works</h2>
+          <p>
+            Every website we build is carefully crafted to help you rank on the first page for the keywords that deliver profitable new customers.
+          </p>
+          <p>
+            We start by learning about your business and your clients. Then we use the best tools in the business to find those critical keywords: Ahrefs, Moz, SEMrush, WhiteSpark, and more. Those keywords are carefully folded into your website design. We only use current onsite SEO best-practices gathered from the best SEO minds in the world.
+          </p>
+          <p>
+            The result is a Milwaukee web design project that works. We get you the best rankings possible. Then, add in expert off-page SEO, pay per click management, and social media work. The result? You’ll have a website that does its job: sending you a steady stream of new customers.
+          </p>
+        </div>
+        <div class="col">
+          <img src="img/image-2.jpg">
+          <h3>Request a qoute | Call Us</h3>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid services">
+      <div class="container">
+        <div class="row">
+          <div class="col-5">
+            <p>OUR</p>
+            <h3>Website Services</h3>
+            <p>
+              Our core practice is helping clients with Milwaukee web design, SEO, and AdWords services. Our goal? To send you a constant stream of new customers. We’re going to learn about the business results you want, and then we’re going to go get it done.
+            </p>
+          </div>
+          <div class="col">
+            <img src="img/icon-1.png">
+            <p>WEB DESIGN</p>
+          </div>
+          <div class="col">
+            <img src="img/icon-2.png">
+            <p>SEO</p>
+          </div>
+          <div class="col">
+            <img src="img/icon-3.png">
+            <p>PAID SEARCH ADS</p>
+          </div>
+          <div class="col">
+            <img src="img/icon-4.png">
+            <p>SOCIAL ADS</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-5">
+            <img src="img/left.png">
+          </div>
+          <div class="col">
+            <h2>Find out how to fix your website SEO!</h2>
+            <p>Enter your website address below to get a full, in-depth SEO report and SEO tips.</p>
+            <form class="form-inline">
+              <div class="form-group">
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+              </div>
+              <button type="submit" class="btn btn-primary">Analyze Now</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container contact-form">
+      <div class="row">
+        <div class="col">
+          <h2>The Heaviside Difference</h2>
+          <p>
+            We are not your average Milwaukee internet marketing firm.
+          </p>
+          <p>
+            Our founder is a physicist. So we use metrics and analytics to drive business results. We will prove you’re getting a return on your investment. A listing in one of the yellow books is not good enough anymore. You have to make sure your website is at least as good as your competition, or you’ll be losing customers to them. Word of mouth and referrals are still important. But if you want to take your business to the next level, you have to win on the internet. We know how to get you there.
+          </p>
+          <p>
+            You’re a business owner. Results matter, and you don’t have time to take on more projects. Let us manage your website and digital marketing, and you can focus on what you do best. Contact us today to start the conversation.
+          </p>
+          <p class="pull-right">
+            <a href="">CHECK OUT OUR ULTIMATE WEBSITE GUIDE →</a>
+          </p>
+        </div>
+        <div class="col-4">
+          <form>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="inputEmail4">Email</label>
+                <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+              </div>
+              <div class="form-group col-md-6">
+                <label for="inputPassword4">Password</label>
+                <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="inputAddress">Address</label>
+              <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+            </div>
+            <div class="form-group">
+              <label for="inputAddress2">Address 2</label>
+              <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="inputCity">City</label>
+                <input type="text" class="form-control" id="inputCity">
+              </div>
+              <div class="form-group col-md-4">
+                <label for="inputState">State</label>
+                <select id="inputState" class="form-control">
+                  <option selected>Choose...</option>
+                  <option>...</option>
+                </select>
+              </div>
+              <div class="form-group col-md-2">
+                <label for="inputZip">Zip</label>
+                <input type="text" class="form-control" id="inputZip">
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input class="form-check-input" type="checkbox"> Check me out
+                </label>
+              </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Sign in</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <img src="img/google-partner.jpg">
+        </div>
+        <div class="col">
+          <img src="img/best-in-search.jpg">
+        </div>
+        <div class="col">
+          <img src="img/top-agency.jpg">
+        </div>
+        <div class="col">
+          <img src="img/bing-ads.jpg">
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid map">
+      <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d46622.78942101801!2d-88.176884!3d43.085086!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc16ebae9704f3d41!2sHeaviside+Group+-+Milwaukee+Web+Design!5e0!3m2!1sen!2sus!4v1511261964902"  height="400" frameborder="0" style="border:0" allowfullscreen="">
+      </iframe>
+    </div>
+    <?php include_once( 'views/layout/footer.php' ); ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="js/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ views/layout/footer.php
@@ -0,0 +1,40 @@
+<div class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h2>CONTACT US</h2>
+        <p>Address</p>
+        <p>21275 GUMINA RD STE 200 , BROOKFIELD, WI 53072</p>
+      </div>
+      <div class="col">
+        <p>Phone no.</p>
+        <p>800-273-2176 | 262-373-8252</p>
+      </div>
+      <div class="col">
+        <h2> Follow Us </h2>
+
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid footer-credits">
+    <div class="container">
+      <div class="row">
+        <div class="col-5">
+          <h6>COPYRIGHT 2017 HEAVISIDE GROUP</h6>
+        </div>
+        <div class="col">
+          <p>Milwaukee Web Design</p>
+        </div>
+        <div class="col">
+          <p>Milwaukee SEO</p>
+        </div>
+        <div class="col">
+          <p>Privacy Policy</p>
+        </div>
+        <div class="col">
+          <p>Privacy Policy</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
