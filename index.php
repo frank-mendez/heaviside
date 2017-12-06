c49758322e06065a61ab2f3a67687583201c476d
unknown <jchristi.austria@gmail.com>
2017-12-06 22:01:22 +0800

    Updated footer.php content. Updated css styling on index.php and footer.php. Added link to FontAwesome cdn used for footer social icons.

--- index.php
+++ index.php
@@ -14,13 +14,14 @@
     <meta property="og:description" content="We&#039;re a Milwaukee web design company without a BORING description. We just get results. You&#039;ll definitely want to check out our free..." />
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <!-- Favicon -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> 
+	<!-- FontAwesome-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+	<!-- Favicon -->
     <link rel="shortcut icon" href="//staging.heavisidegroup.com/wp-content/uploads/2015/05/favicon.ico">
     <!-- Custom CSS -->
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    
   </head>
   <body>
     <?php include_once( 'views/layout/navbar.php' ); ?>
@@ -34,88 +35,106 @@
     </div>
     <div id="intro" class="container">
       <div class="row">
-        <div class="col">
+        <div class="col-md-6">
           <span class="small-header">WE ARE A</span>
 		  <div class="title">
-			  <h3>Milwaukee web design company</h3>
+			  <h3 style="color: #100e53;">Milwaukee web design company</h3>
 			  <span style="font-weight: 300; color: #292624">that helps you </span>
 			  <span style="color: #0b9ed0; font-weight: 700;">get more customers.</h2>
 		  </div>
         </div>
-        <div class="col intro-list">
+        <div class="col-md-6 intro-list">
           <ul>
             <li>You don’t need an expensive brochure that no one sees.</li>
             <li> You need your website working 24/7 to help your customers find you.</li>
           </ul>
         </div>
       </div>
+	  
       <div class="row expertise">
-        <div class="col">
+        <div class="col-md-8">
           <img src="img/image-1.png" class="box-shadow">
         </div>
-        <div class="col circular-img">
+        <div class="col-md-3 circular-img">
           <img src="img/expertise-2017-1-1.png" class="box-shadow">
           <img src="img/expertise-2016-1-1.png" class="box-shadow">
         </div>
       </div>
+	  
       <div class="row">
-        <div class="col text-justify">
-          <p>WHAT WE DO</p>
-          <h2>Milwaukee Digital Marketing That Works</h2>
-          <p>
-            Every website we build is carefully crafted to help you rank on the first page for the keywords that deliver profitable new customers.
-          </p>
-          <p>
-            We start by learning about your business and your clients. Then we use the best tools in the business to find those critical keywords: Ahrefs, Moz, SEMrush, WhiteSpark, and more. Those keywords are carefully folded into your website design. We only use current onsite SEO best-practices gathered from the best SEO minds in the world.
-          </p>
-          <p>
-            The result is a Milwaukee web design project that works. We get you the best rankings possible. Then, add in expert off-page SEO, pay per click management, and social media work. The result? You’ll have a website that does its job: sending you a steady stream of new customers.
-          </p>
-        </div>
-        <div class="col">
-          <img src="img/image-2.jpg" class="request-a-quote-img">
-          <h3>Request a qoute | Call Us</h3>
-        </div>
-      </div>
-    </div>
-    <div class="container-fluid services text-justify">
-      <div class="container">
-        <div class="row">
-          <div class="col-5">
-            <p>OUR</p>
-            <h3>Website Services</h3>
-            <p>
-              Our core practice is helping clients with Milwaukee web design, SEO, and AdWords services. Our goal? To send you a constant stream of new customers. We’re going to learn about the business results you want, and then we’re going to go get it done.
-            </p>
-          </div>
-		  <!--end of Our Services description -->
-		  
-			  <div class="col"></div>
-			  <div class="col">	<!-- needs tooptimize -->
-				<img src="img/icon-1.png" >
-				<p class="text-center">WEB DESIGN</p>
+			<div class="col-md-6 what-we-do text-justify">
+				<div class="section-padding">
+			  <p class="small-header">WHAT WE DO</p>
+			  <h3 class="title">Milwaukee Digital Marketing That Works</h3>
 			  </div>
-			  <div class="col">
-				<img src="img/icon-2.png" >
-				<p class="text-center">SEO</p>
-			  </div>
-			  <div class="col">
-				<img src="img/icon-3.png">
-				<p class="text-center">PAID SEARCH ADS</p>
-			  </div>
-			  <div class="col">
-				<img src="img/icon-4.png">
-				<p class="text-center">SOCIAL ADS</p>
-			  </div>
-		  
-		
-		
+			  
+			  <p class="default-paragraph">
+				Every website we build is carefully crafted to help you rank on the first page for the keywords that deliver profitable new customers.
+			  </p>
+			  <p class="default-paragraph">
+				We start by learning about your business and your clients. Then we use the best tools in the business to find those critical keywords: Ahrefs, Moz, SEMrush, WhiteSpark, and more. Those keywords are carefully folded into your website design. We only use current onsite SEO best-practices gathered from the best SEO minds in the world.
+			  </p>
+			  <p class="default-paragraph">
+				The result is a Milwaukee web design project that works. We get you the best rankings possible. Then, add in expert off-page SEO, pay per click management, and social media work. The result? You’ll have a website that does its job: sending you a steady stream of new customers.
+			  </p>
+			</div>
+			<div class="col-md-6 title-padding">
+				<img src="img/image-2.jpg" class="request-a-quote-img">
+				
+				<h3 class="custom-heading">Request a qoute </h3><h3 style="display: inline-block">&nbsp;| Call Us</h3>
+
+				<div class="learn-more">
+					<a href="#">Learn More</a>
+				</div>
+					
+			</div>
+			
+			
         </div>
       </div>
     </div>
-    <div class="container-fluid contact">
+    
+	<div class="container-fluid services text-justify">
       <div class="container">
-        <div id="analyze-seo" class="row">
+        <div class="row h-100">
+			<div class="col-12">
+			    <div class="row h-100 justify-content-center align-items-center">
+				  <div class="col-5">
+					<p style="margin-bottom: 0;">OUR</p>
+					<h3>Website Services</h3>
+					<p class="default-paragraph">
+					  Our core practice is helping clients with Milwaukee web design, SEO, and AdWords services. Our goal? To send you a constant stream of new customers. We’re going to learn about the business results you want, and then we’re going to go get it done.
+					</p>
+				  </div>
+				  <div class="col-6 offset-1 services-icons">
+					
+					<div class="row">
+						<div class="col">	
+						<img src="img/icon-1.png" >
+						<p>WEB DESIGN</p>
+					  </div>
+					  <div class="col">
+						<img src="img/icon-2.png" >
+						<p>SEO</p>
+					  </div>
+					  <div class="col">
+						<img src="img/icon-3.png">
+						<p>PAID SEARCH ADS</p>
+					  </div>
+					  <div class="col">
+						<img src="img/icon-4.png">
+						<p>SOCIAL ADS</p>
+					  </div>
+				  </div>
+				</div>
+			</div>
+		  </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid analyze-seo">
+      <div class="container">
+        <div class="row h-100 justify-content-center align-items-center">
           <div class="col-5">
             <img src="img/left.png">
           </div>
@@ -136,16 +155,16 @@
       <div class="row ">
         <div class="col">
           <h2>The Heaviside Difference</h2>
-          <p>
+          <p style="color:white;">
             We are not your average Milwaukee internet marketing firm.
           </p>
-          <p>
+          <p style="color:white;">
             Our founder is a physicist. So we use metrics and analytics to drive business results. We will prove you’re getting a return on your investment. A listing in one of the yellow books is not good enough anymore. You have to make sure your website is at least as good as your competition, or you’ll be losing customers to them. Word of mouth and referrals are still important. But if you want to take your business to the next level, you have to win on the internet. We know how to get you there.
           </p>
-          <p>
+          <p style="color:white;">
             You’re a business owner. Results matter, and you don’t have time to take on more projects. Let us manage your website and digital marketing, and you can focus on what you do best. Contact us today to start the conversation.
           </p>
-          <p class="pull-right">
+          <p class="pull-right" >
             <a href="">CHECK OUT OUR ULTIMATE WEBSITE GUIDE →</a>
           </p>
         </div>
--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,40 +1,72 @@
 <div class="footer">
   <div class="container">
-    <div class="row">
-      <div class="col">
-        <h2>CONTACT US</h2>
-        <p>Address</p>
-        <p>21275 GUMINA RD STE 200 , BROOKFIELD, WI 53072</p>
-      </div>
-      <div class="col">
-        <p>Phone no.</p>
-        <p>800-273-2176 | 262-373-8252</p>
-      </div>
-      <div class="col">
-        <h2> Follow Us </h2>
+   	<div class="row">
+		<div class="col-12">
+			<div class="row footer-contact-info">
+			  <div class="col-8">
+				<h6> Contact Us </h6>
+				<div class="row">
+					<div class="col">	
+						<span>Address</span>
+						<p>21275 GUMINA RD STE 200 , BROOKFIELD, WI 53072</p>
+					</div>
+					<div class="col">	
+						
+						<span>Phone no.</span>
+						<p>800-273-2176 | 262-373-8252</p>
+					</div>
+				</div>
+				
+			  </div>
+			  <div class="col-4">
+				<h6> Follow Us </h6>
+				<div class="row social-icons">
+					<div class="col">	
+						<a href="#" class="facebook"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+					</div>
+					<div class="col">
+						<a href="#" class="twitter"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+					</div>
+					<div class="col">
+						<a href="#" class="linkedin"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+					</div>
+					<div class="col">
+						<a href="#" class="gplus"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	  </div>
+	</div>	
+  </div>
+</div>
 
-      </div>
-    </div>
-  </div>
-  <div class="container-fluid footer-credits">
-    <div class="container">
-      <div class="row">
-        <div class="col-5">
-          <h6>COPYRIGHT 2017 HEAVISIDE GROUP</h6>
-        </div>
-        <div class="col">
-          <p>Milwaukee Web Design</p>
-        </div>
-        <div class="col">
-          <p>Milwaukee SEO</p>
-        </div>
-        <div class="col">
-          <p>Privacy Policy</p>
-        </div>
-        <div class="col">
-          <p>Privacy Policy</p>
-        </div>
-      </div>
-    </div>
-  </div>
+<div class="subfooter">
+	<div class="container-fluid h-100">
+	   <div class="container h-100">
+		  <div class="row h-100 justify-content-center align-items-center subfooter-content">
+			<div class="col-5">
+				<h6>COPYRIGHT 2017 HEAVISIDE GROUP</h6>
+			</div>
+			<div class="col-6 offset-1 footer-menu text-right">
+				<ul class="list-inline">
+					<li class="list-inline-item">
+						<a href="">Milwaukee&nbsp;Web&nbsp;Design</a>
+					</li>
+					<li class="list-inline-item">
+						<a href="">Milwaukee SEO</a>
+					</li>
+					<li class="list-inline-item">
+						<a href="">Privacy Policy</a>
+					</li>
+					<li class="list-inline-item">
+						<a href="">Sitemap</a>
+					</li>
+				</ul>
+				
+			</div>
+			
+		  </div>
+		</div>
+	</div>
 </div>
