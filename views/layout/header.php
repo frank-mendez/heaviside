d7a3c89a4ae0e6a05ec9f6eade9b964d383e870f
Frank Mendez <Frank Mendez>
2017-12-11 17:06:21 +0800

    feat(header): Added header for dynamic purposes

---
+++ views/layout/header.php
@@ -0,0 +1,23 @@
+<head>
+    <title>Milwaukee Web Design Company and Digital Marketing Experts</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- SEO -->
+    <meta name="description" content="We&#039;re a Milwaukee web design company without a BORING description. We just get results. You&#039;ll definitely want to check out our free..."/>
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Milwaukee Web Design Company and Digital Marketing Experts" />
+    <meta property="og:description" content="We&#039;re a Milwaukee web design company without a BORING description. We just get results. You&#039;ll definitely want to check out our free..." />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> 
+    <!-- FontAwesome-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="//staging.heavisidegroup.com/wp-content/uploads/2015/05/favicon.ico">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
