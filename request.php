f9e8929d55fb487dc82d3d4ef5a891e3fca983f2
Frank Mendez <Frank Mendez>
2017-12-11 22:00:00 +0800

    feat(page): Added request page

---
+++ request.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <?php include_once( 'views/layout/header.php' ); ?>
+  <body>
+    <?php include_once( 'views/layout/navbar.php' ); ?>
+    <!-- Content Here -->
+    <div class="container-fluid no-padding">
+    	<img class="img-fluid" src="img/heaviside-contact-us-1017.jpg">
+    </div>	
+  	<div class="container">
+  		<div class="row">
+  			<div class="col center margin-top-fifty">
+  				<h3>Request a Qoute</h3>
+  				<p>
+  					Enter your information in the form and someone will reach out to you shortly.<br>
+  					You can also call us toll free at <a href="">800-273-2176</a>, or locally at <a href="">262-373-8285</a>.
+  					We look forward to hearing from you!
+  				</p>
+  			</div>
+  		</div>
+  		<div class="row">
+  			<div class="col-8">
+  				<form>
+  					<div class="row margin-top-fifteen">
+  						<div class="col">
+					      <input type="text" class="form-control" placeholder="Name">
+					    </div>
+					    <div class="col">
+					      <input type="text" class="form-control" placeholder="Email">
+					    </div>
+					    <div class="col">
+					      <input type="text" class="form-control" placeholder="Subject">
+					    </div>
+  					</div>
+  					<div class="row margin-top-fifteen">
+  						<div class="col">
+  							<textarea class="form-control" rows="10" placeholder="Please describe your project request of the service you are interested in"></textarea>
+  						</div>
+  					</div>
+  					<div class="row margin-top-fifteen">
+  						<div class="col">
+  							<button type="button" class="btn btn-outline-primary">Submit</button>
+  						</div>
+  					</div>
+  				</form>
+  			</div>
+  			<div class="col margin-top-fifteen">
+  				<div class="row">
+  					<h3><b>Heaviside  Group</b></h3>
+  					<p>21275 GUMINA RD STE 200 , BROOKFIELD, WI 53072</p>
+  					<p>800-273-2176 | 262-373-8252</p>
+  					<p>
+  						<a href="">info@heavisidegroup.com</a><br>
+  						<a href="">www.heavisidegroup.com</a><br>
+  						<a href="">www.heavisidegroup.digital</a>
+  					</p>
+  				</div>
+  			</div>
+  		</div>
+  	</div>
+    <?php include_once( 'views/layout/footer.php' ); ?>
+  </body>
+</html>
