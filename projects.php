cb2de7c1248f9f62a0b2c262c1176510c4f06688
Frank Mendez <Frank Mendez>
2017-12-12 16:45:55 +0800

    feat(project): Added project page

---
+++ projects.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+  <?php include_once( 'views/layout/header.php' ); ?>
+  <body>
+    <?php include_once( 'views/layout/navbar.php' ); ?>
+    <!-- Content Here -->
+  	<div class="container-fluid no-padding">
+  		<img class="img-fluid" src="img/heaviside-portfolio-1013_02.png">
+  	</div>
+  	<div class="container-fluid experts">
+      <div class="container">
+        <div class="row">
+          <div class="col white center">
+            <h2><b>More Projects? check out below!</b></h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container margin-top-fifty">
+    	<div class="row">
+    		<div class="col center">
+    			<h4>U-SOLVE</h4>
+    			<p><b>Services - Homepage Redesign - SEO - ADS</b></p>
+    			<p>
+    				These are some of our recent projects. Sometimes the website render bot does not handle parallax properly, so click through to the actual website to see the live version.
+    			</p>
+    			<p><b>Related Projects</b></p>
+    		</div>
+    		<div class="col-7">
+    			<a href="">
+    				<img class="img-fluid" src="img/heaviside-portfolio-1013_05.png">
+    			</a>
+    		</div>
+    	</div>
+    	<div class="row margin-top-fifty">
+    		<div class="col-7">
+    			<a href="">
+    				<img class="img-fluid" src="img/heaviside-portfolio-1013_09.png">
+    			</a>
+    		</div>
+    		<div class="col center margin-top-fifty">
+    			<h4>COMPUTERS</h4>
+    			<p>
+    				These are some of our recent projects. Sometimes the website render bot does not handle parallax properly, so click through to the actual website to see the live version.
+    			</p>
+    			<p><a href="">Link here</a></p>
+    		</div>
+    	</div>
+    	<div class="row margin-top-fifty">
+    		<div class="col center margin-top-fifty">
+    			<h4>HILLCREST RV RESORT</h4>
+    			<p>
+    				These are some of our recent projects. Sometimes the website render bot does not handle parallax properly, so click through to the actual website to see the live version.
+    			</p>
+    			<p><a href="">Link here</a></p>
+    		</div>
+    		<div class="col-7">
+    			<a href="">
+    				<img class="img-fluid" src="img/heaviside-portfolio-1013_13.png">
+    			</a>
+    		</div>
+    	</div>
+    	<div class="row margin-top-fifty">
+    		<div class="col-7">
+    			<a href="">
+    				<img class="img-fluid" src="img/heaviside-portfolio-1013_15.png">
+    			</a>
+    		</div>
+    		<div class="col center margin-top-fifty">
+    			<h4>BARK RIVER</h4>
+    			<p>
+    				These are some of our recent projects. Sometimes the website render bot does not handle parallax properly, so click through to the actual website to see the live version.
+    			</p>
+    			<p><a href="">Link here</a></p>
+    		</div>
+    	</div>
+    </div>
+    <div class="container-fluid experts margin-top-fifty">
+      <div class="container">
+      	<h3 class="center white"><b>Other Projects</b></h3>
+        <div class="row margin-top-fifty">
+          <div class="col">
+          	<a href="">
+          		<img class="img-fluid" src="img/heaviside-portfolio-1013_19.png">
+          	</a>
+          </div>
+          <div class="col">
+          	<a href="">
+          		<img class="img-fluid" src="img/heaviside-portfolio-1013_21.png">
+          	</a>
+          </div>
+          <div class="col">
+          	<a href="">
+          		<img class="img-fluid" src="img/heaviside-portfolio-1013_23.png">
+          	</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php include_once( 'views/layout/footer.php' ); ?>
+  </body>
+</html>
