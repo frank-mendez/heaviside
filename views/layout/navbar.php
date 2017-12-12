caf1905466f38f37270ede0c7be74938b3ab0bec
Frank Mendez <Frank Mendez>
2017-12-12 12:00:27 +0800

    feat(js): Added icon and back to top button,localize jquery

--- views/layout/navbar.php
+++ views/layout/navbar.php
@@ -29,17 +29,17 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="seo">
+        <a class="nav-link" href="seo"><i class="fa fa-line-chart" aria-hidden="true"></i> 
         SEO <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="ppc">PPC</a>
+        <a class="nav-link" href="ppc"><i class="fa fa-bullseye" aria-hidden="true"></i> PPC</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Web Design</a>
+        <a class="nav-link" href="#"><i class="fa fa-desktop" aria-hidden="true"></i> Web Design</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="request">Request A Quote</a>
+        <a class="nav-link" href="request"><i class="fa fa-file-text" aria-hidden="true"></i> Request A Quote</a>
       </li>
     </ul>
   </div>
