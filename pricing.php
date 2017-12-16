6ee4f72b97574e9eea969e90a923fc177ff439e8
Chris Sloane <chris@heavisidegroup.com>
2017-12-16 12:39:41 -0600

    rewrite sales copy for this page

--- pricing.php
+++ pricing.php
@@ -8,44 +8,45 @@
   		<div class="container facebook-ads pricing">
   			<div class="row">
   				<div class="col">
-  					<h3 class="white">Hey It’s Chris from Heaviside Group</h3>
+  					<h3 class="white">Hi there - it’s Chris from Heaviside Group.</h3>
   					<p>
   						Thanks for requesting information about our pricing. 
   					</p>
   					<p>
-  						At Heaviside Group, we have one mission for every customer: deliver a steady stream of profitable new customers. Every project and service we offer are designed to do exactly this. I would be glad to provide a complete proposal and exact quote after we talk about your needs, but here’s some basic pricing information.
+  						At Heaviside Group, we have one mission for every customer: deliver a steady stream of profitable new customers. Every project and service we offer is designed to do precisely this. We would be glad to provide a complete proposal and exact quote after we discuss your needs, but here’s some basic pricing guidelines.
+  					</p>
+  					<p style="font-weight:bold">
+  						A website for a small or medium-sized business that is custom designed, strategically built to turn visitors into customers, and optimized for smartphones usually costs between $3k and $6k. </p>
+              <p>
+              85-90% of small business websites should fall into this range. A <strong>large website</strong> that involves hundreds of pages or very advanced custom features usually costs <strong>between $10k and $25k</strong>. Examples might be sites doing e-commerce, sites that handle employee-related tasks like timekeeping, in-depth customer-facing quoting pages, etc. If you’ve seen quotes in this high range for a website and you don’t have these kinds of advanced requirements, chances are you are really being overcharged to support a bloated agency staff.
   					</p>
   					<p>
-  						A website for a small or medium-sized business that is custom designed, strategically built to turn visitors into customers, and optimized for
-smartphones usually costs between $3k and $6k. 85-90% of small business websites should fall into this range. A large website that involves hundreds of pages or very advanced custom features usually costs between $10k and $25k. Examples might be sites doing e-commerce, sites that handle employee-related tasks like timekeeping, in-depth customer-facing quoting pages, etc. If you’ve seen quotes in this high range for a website and you don’t have these kinds of advanced requirements, chances are you are really being charged to support a bloated agency staff.
+  						Whatever kind of site you need, it will come with The Heaviside 100% Guarantee: <em>You’ll 100% love the result or else we’ll 100% redo it</em>.
   					</p>
   					<p>
-  						Whatever kind of site you need, it will come with The Heaviside You’ll Love It or Else! Guarantee. You’ll 100% love the result or else we’ll 100% redo it/refund you.
-  					</p>
-  					<p>
-  						Our online marketing services can cost anywhere from $199/month to $2,999/month, and can include the following:
+  						Our online marketing services can cost anywhere from $299/month to $2,999/month, and can include the following:
   					</p>
   					<p class=" margin-fifty">
-  						 <span class="highlight"><i class="fa fa-check" aria-hidden="true"></i> Managed Web Hosting, Email, Maintenance, and Security</span><br>
-includes web and email hosting, as well as site maintenance and security
+  						 <span class="highlight"><i class="fa fa-check" aria-hidden="true"></i> Managed Website - $299/month</span><br>
+includes web and email hosting, site maintenance and security, and most updates and changes short of a rebuild. No hourly nickel and diming from us!
   					</p>
   					<p class=" margin-fifty">
-  						 <span class="highlight"><i class="fa fa-check" aria-hidden="true"></i> Managed Local Business Website</span><br>
-flat rate includes website updates, local SEO, analytics setup and reports, social media integration, and online reviews management
+  						 <span class="highlight"><i class="fa fa-check" aria-hidden="true"></i> Managed Digital Marketing - $1,499/month</span><br>
+done for you, <em>profitable</em> advertising. Google AdWords, Facebook Ads, Yelp -- wherever your customers are, we'll help them find you. Includes consulting to help you best define your offer, landing page builds, ad builds and management, analytics and conversion testing and optimization, and much more. Don't leave this to pretenders (lots of them hire us to do it for <em>their</em> customers) - we serve clients all over the world including white-label services to other agencies. We are also the agency of record for Growth Geeks for digital marketing services and manage dozens of live campaigns with hundreds of thousands of monthly ad spend. 
   					</p>
   					<p class=" margin-fifty">
-  						 <span class="highlight"><i class="fa fa-check" aria-hidden="true"></i> Managed Email Marketing Services</span><br>
-email is still the highest ROI marketing approach, by far. We’ll help you set it up and manage it
+  						 <span class="highlight"><i class="fa fa-check" aria-hidden="true"></i> Managed Search Engine Optimization - $1,499/month</span><br>
+whether you are a local business that needs more customers or an international brand seeking to grow organic traffic across multiple geos, we know how to help. We have customers all over the world that rely on us to deliver profitable traffic (other agencies rely on us too for <em>their</em> clients). If you want SEO that doesn't work, hire your average web design agency. We don't listen to the "SEO experts" who parrot whatever nonsense Google is putting out there to stop SEOs from improving rankings -- we <em>test</em> different strategies to see what works and only listen to other SEOs that do the same and back it up with data. Want to know who the best SEOs in the world are? They aren't writing articles for Search Engine Land, they are competing against each other to rank websites for keywords that drives millions of dollars in affiliate revenue for them every year. That's who we watch and learn from, and we use those proven strategies for our own clients.
   					</p>
   					<p class=" margin-fifty">
-  						 <span class="highlight"><i class="fa fa-check" aria-hidden="true"></i> Managed Social Media Services</span><br>
-unique daily content for your business delivered directly to Facebook and Twitter
+  						 <span class="highlight"><i class="fa fa-check" aria-hidden="true"></i> Total Managed Digital - from $2,999</span><br>
+this is everything <em>and</em> the kitchen sink. Want us to handle everything for you? This is for you. Includes all of the managed services above, as well as strategic consulting and other related marketing services, such as graphic design, branding, email marketing, and more. The other guys charge $5,000 per month or more and won't deliver nearly the same results. Hire the team other agencies rely on - <strong>us</strong>. 
   					</p>
   					<p>
   						We call them all managed for a reason–we get you out of the website and internet marketing business and take accountability for results. Whatever combination you choose, it will come with The Heaviside Results Matter… Or It’s Free! Guarantee. If you’re not seeing the results you want in 90 days we’ll work up to another 90 days for free to make sure you are. And if you’re still not satisfied? You can walk away because we don’t make you sign service contracts.
   					</p>
   					<p>
-  						Many agencies with bloated staffs do exactly that. We don’t have to. We know what clients are a best-fit for us, what services will make the biggest impact for them, and how to deliver them efficiently and profitably. If you are looking for rebranding, “traditional” advertising management, or print/show work, we are not who you are looking for.
+  						Many agencies with bloated staffs do exactly that. We don’t have to. We know what clients are a best-fit for us, what services will make the biggest impact for them, and how to deliver them efficiently and profitably. If you are looking for “traditional” advertising management, or print/show work, we are not who you are looking for.
   					</p>
   					<p>
   						But if you’re a small- to medium-size business owner that primarily serves a local customer base and you don’t have the time or expertise to do this, we are built to help you grow.
